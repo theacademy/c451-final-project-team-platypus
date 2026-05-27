18887f45e83226ed0329a43b694cb9ac200394ab
BetterBite <38665746+BetterBite@users.noreply.github.com>
2026-05-27 16:26:35 +0100

    initial commit

---
+++ stocksimulator/src/main/java/mthree/stocksimulator/StockSimulator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mthree.stocksimulator;
+
+/**
+ *
+ * @author gabri
+ */
+public class StockSimulator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
